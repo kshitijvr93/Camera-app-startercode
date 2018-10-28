15327d658ddf317fb0b5f1137dcac16112f80fc7
Kshitij Ranganath <kshitijvr93@gmail.com>
2018-10-27 23:18:03 -0400

    Working Image Picker

--- App.js
+++ App.js
@@ -1,29 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button
+} from 'react-native';
+import ImagePicker from "react-native-image-picker";
+
+
+export default class App extends Component {
+
+  state = {
+    pickedImage: null
+  }
+
+  reset = () => {
+    this.setState({
+      pickedImage: null
+    });
+  }
+
+  /**
+ * The first arg is the options object for customization (it can also be null or omitted for default options),
+ * The second arg is the callback which sends object: response (more info below in README)
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+  pickImageHandler = () => {
+    ImagePicker.showImagePicker({title: "Pick an Image", maxWidth: 800, maxHeight: 600}, res => {
+      if (res.didCancel) {
+        console.log("User cancelled!");
+      } else if (res.error) {
+        console.log("Error", res.error);
+      } else {
+        this.setState({
+          pickedImage: { uri: res.uri }
+        });
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+      }
+    });
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  resetHandler = () =>{
+    this.reset();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <Text style={styles.textStyle}>Pick Image From Camera and Gallery </Text>
+        <View style={styles.placeholder}>
+          <Image source={this.state.pickedImage} style={styles.previewImage} />
+        </View>
+        <View style={styles.button}>
+
+          <Button title="Pick Image" onPress={this.pickImageHandler} />
+
+          <Button title="Reset" onPress={this.resetHandler} />
+
+        </View>
       </View>
     );
   }
@@ -31,19 +66,31 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems:"center"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  textStyle: {
+    fontWeight:"bold",
+    fontSize:30,
+    textAlign:"center",
+    color:"red",
+    marginTop:10
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  placeholder: {
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "#eee",
+    width: "70%",
+    height: 280,
+    marginTop:50,
   },
+  button: {
+    width: "80%",
+    marginTop:20,
+    flexDirection:"row",
+    justifyContent: "space-around"
+  },
+  previewImage: {
+      width: "100%",
+      height: "100%"
+  }
 });
